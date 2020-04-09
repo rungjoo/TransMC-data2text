0ef52571adea28d3a933caff25f1f1b2410d80c6
rungjoo <mexade92@gmail.com>
2020-04-09 12:14:05 +0000

    commit

---
+++ gpt_model/base-large.py
@@ -0,0 +1,72 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import sys
+import math
+import pandas as pd
+
+from transformers import *
+
+class mymodel(nn.Module):
+    def __init__(self):
+        super(mymodel, self).__init__()
+        dataset = pd.read_csv('dataset/trainset.csv')
+        columns = dataset.columns
+        conditions = dataset[columns[0]]
+
+        typ_list = {}
+        for k in range(len(conditions)):
+            cond_set = conditions[k].split(',')
+            for m in range(len(cond_set)):
+                cond_set[m] = cond_set[m].strip()
+                pos = cond_set[m].index('[')
+                if cond_set[m][:pos] in typ_list.keys():
+                    typ_list[cond_set[m][:pos]].add(cond_set[m][pos+1:-1])
+                else:            
+                    typ_list[cond_set[m][:pos]] = {cond_set[m][pos+1:-1]}        
+
+        condition_token = []
+        v_num = 0
+        for k, v in typ_list.items():
+            v_num += len(v)
+            condition_token.append('<'+k+'>')
+#             print(k, len(v))
+#         print(len(typ_list.keys()), v_num)
+#         print(condition_token)                            
+        
+        model_class, tokenizer_class, pretrained_weights = (GPT2Model, GPT2Tokenizer, 'gpt2-large')
+        self.tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
+
+        special_tokens = {'bos_token': '<START>', 'additional_special_tokens': condition_token}
+        self.tokenizer.add_special_tokens(special_tokens)
+        self.bert_model = model_class.from_pretrained(pretrained_weights)
+        self.bert_model.resize_token_embeddings(len(self.tokenizer))
+
+        
+        self.emb_dim = 1280
+        self.vocab_num = len(self.tokenizer)
+        
+        self.matrix = nn.Linear(self.emb_dim, self.vocab_num)
+        
+#         self.model_params = list(self.matrix.parameters())
+        
+
+    """Modeling"""
+    def model_feeding(self, input_ids):
+        output_vector = self.bert_model(input_ids)[0]
+        voacb_logit = self.matrix(output_vector)
+        
+        return voacb_logit
+        
+        
+    def cls_loss(self, model_out, label_ids):
+        """
+        model_out: (input_idxs, vocab_num) (logits)
+        label_ids: (label_idxs)        
+        """      
+        pred_out = model_out[-len(label_ids):]
+        loss = F.cross_entropy(pred_out, label_ids)
+        
+        return loss
+        
---
+++ train_large.py
@@ -0,0 +1,108 @@
+from tqdm import tqdm
+import os
+import random
+import torch
+
+from model_large import *
+my_model = mymodel().cuda()
+my_model.train()
+print('ok')
+
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+class e2eDataset(Dataset):
+    def __init__(self, csv_file, tokenizer):
+        """
+        Args:
+            csv_file (string): csv 파일의 경로
+        """
+        self.dataset = pd.read_csv(csv_file)
+        self.columns = self.dataset.columns
+        self.conditions = self.dataset[self.columns[0]]
+        self.sentences = self.dataset[self.columns[1]]
+        self.tokenizer = tokenizer
+        
+        self.typ_list = {}
+        for k in range(len(self.conditions)):
+            cond_set = self.conditions[k].split(',')
+            for m in range(len(cond_set)):
+                cond_set[m] = cond_set[m].strip()
+                pos = cond_set[m].index('[')
+                if cond_set[m][:pos] in self.typ_list.keys():
+                    self.typ_list[cond_set[m][:pos]].add(cond_set[m][pos+1:-1])
+                else:            
+                    self.typ_list[cond_set[m][:pos]] = {cond_set[m][pos+1:-1]}        
+
+    def __len__(self):
+        return len(self.conditions)
+
+    def __getitem__(self, idx):
+        cond = self.conditions[idx]
+        cond_set = cond.split(',')
+        condition_string = ''
+        for m in range(len(cond_set)):
+            cond_set[m] = cond_set[m].strip()
+            pos = cond_set[m].index('[')
+            
+            condition_string += '<' + cond_set[m][:pos] + '>' + cond_set[m][pos+1:-1] + ' '
+        
+        sen = self.sentences[idx]
+        input_string = condition_string + '<START>' + sen
+        input_ids = torch.tensor(self.tokenizer.encode(input_string, add_special_tokens=True))
+        
+        label_string = sen + ' <|endoftext|>'
+        label_ids = torch.tensor(self.tokenizer.encode(label_string, add_special_tokens=True))
+
+        return input_ids, label_ids
+
+## finetune bert_model
+def main():
+    e2e_dataset = e2eDataset(csv_file='dataset/trainset.csv', tokenizer=my_model.tokenizer)
+    dataloader = DataLoader(e2e_dataset, batch_size=1, shuffle=True, num_workers=4)    
+
+    # Parameters:
+    epoch = 5
+    lr = 1e-5 # 2e-5
+    max_grad_norm = 10
+    num_training_steps = len(e2e_dataset)*epoch
+    num_warmup_steps = len(e2e_dataset)
+
+    ### In Transformers, optimizer and schedules are splitted and instantiated like this:
+    optimizer = torch.optim.AdamW(my_model.parameters(), lr=lr, eps=1e-06, weight_decay=0.01)
+#     optimizer = AdamW(my_model.parameters(), lr=lr, correct_bias=False)  # To reproduce BertAdam specific behavior set correct_bias=False
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=num_training_steps)  # PyTorch scheduler
+    
+    for epoch in tqdm(range(6)):
+        for i_batch, sample_batched in enumerate(dataloader):
+            input_ids = sample_batched[0].squeeze(0).cuda()
+            label_ids = sample_batched[1].squeeze(0).cuda()
+
+            model_out = my_model.model_feeding(input_ids) # (batch, seq_len, emb_dim)    
+
+            loss = my_model.cls_loss(model_out, label_ids)
+
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(my_model.parameters(), max_grad_norm)  # Gradient clipping is not in AdamW anymore (so you can use amp without issue)
+            optimizer.step()
+            scheduler.step()
+            optimizer.zero_grad()    
+        
+        """savining point"""
+#         if (epoch+1)%2 == 0:
+        save_model(epoch+1)
+    save_model('final') # final_model     
+    save_path = 'gen_model/large2/final/'
+    my_model.bert_model.save_pretrained(save_path)
+    my_model.tokenizer.save_pretrained(save_path)
+        
+        
+def save_model(iteration):
+    save_path = 'gen_model/large2/'+str(iteration)+'/'
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    torch.save(my_model.state_dict(), save_path+'model')
+    
+
+if __name__ == '__main__':
+    torch.cuda.empty_cache()
+    main()        
